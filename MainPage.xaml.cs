000355ad335b9e39345d2fae4351033185a06627
TamHuynh <tamhuynh.msp@outlook.com>
2015-08-28 15:10:40 +0700

    Toastinet Example release

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TestToastinetProject.Resources;
+
+namespace TestToastinetProject
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageBoxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a simple message box");
+        }
+
+        private void ToastBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyToast.Message = "This is a toast message";
+            
+        }
+    }
+}
